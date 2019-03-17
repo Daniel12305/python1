ea996ac5c305df7c8b67f81b9cf72bbda8087e77
Daniel12305 <47741622+Daniel12305@users.noreply.github.com>
2019-03-17 22:06:35 +0100

    Update input.py

--- romanNumber/input.py
+++ romanNumber/input.py
@@ -3,6 +3,5 @@ from Funktion2zusammengefasst import romanNumber
 while True:
     try: input = raw_input
     except NameError: pass
-    #print("Hi " + input("Say something: "))
     gewuenschteZahl = int(input("Arabische Zahl eingeben "))
     print("Römische Zahl: " + romanNumber(gewuenschteZahl))
